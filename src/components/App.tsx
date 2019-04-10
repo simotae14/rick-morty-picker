a4cb7ac541c0997654a7e9171da2790702e7c372
Simone Taeggi <startae14@gmail.com>
2019-04-11 01:01:52 +0200

    Show the episodes and css

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,24 @@
 import React from 'react'
 import { Store } from '../store/Store';
 
+// interface for episode
+interface IEpisode {
+    airdate: string
+    airstamp: string
+    airtime: string
+    id: number
+    image: {
+        medium: string
+        original: string
+    }
+    name: string
+    number: number
+    runtime: number
+    season: number
+    summary: string
+    url: string
+}
+
 export default function App(): JSX.Element {
     const { state, dispatch } = React.useContext(Store);
 
@@ -20,15 +38,33 @@ export default function App(): JSX.Element {
             payload: dataJSON._embedded.episodes
         });
     }
-    console.log(state);
     return (
         <React.Fragment>
-            <h1>
-                Rick and Morty
-            </h1>
-            <p>
-                Pick your favourite episode!!!
-            </p>
+            <header className="header">
+                <h1>
+                    Rick and Morty
+                </h1>
+                <p>
+                    Pick your favourite episode!!!
+                </p>
+            </header>
+            <section className="episode-layout">
+                {
+                    state.episodes.map((episode: IEpisode) => {
+                        return (
+                            <section key={episode.id} className="episode-box">
+                                <img src={episode.image.medium} alt={`Rick and Morty ${episode.name}`} />
+                                <div>
+                                    { episode.name }
+                                </div>
+                                <section>
+                                    Season: { episode.season } Number: { episode.number }
+                                </section>
+                            </section>
+                        );
+                    })
+                }
+            </section>
         </React.Fragment>
     );
 }
--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,13 @@ const rules = [
         test: /\.tsx?/,
         exclude: /node_modules/,
         loader: 'babel-loader'
+    },
+    {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
     }
 ];
 
